7e646e1c124820d48b1f8361e406aff4fc8b570b
SAMIUR <samiurkhan94@gmail.com>
2022-08-16 22:13:51 +0600

    db connection stabilized

--- src/career/config/config.service.ts
+++ src/career/config/config.service.ts
@@ -38,6 +38,7 @@ class ConfigService {
       username: this.getValue('POSTGRES_USER'),
       password: this.getValue('POSTGRES_PASSWORD'),
       database: this.getValue('POSTGRES_DATABASE'),
+      synchronize: true,
 
       entities: ['dist/**/*.entity.js'],
 
