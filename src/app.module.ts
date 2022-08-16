eac788e76e7964c7e22f24109107f5c2ffc456bd
SAMIUR <samiurkhan94@gmail.com>
2022-08-16 21:52:10 +0600

    db initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CareerModule } from './career/career.module';
 
 @Module({
-  imports: [],
+  imports: [CareerModule],
   controllers: [AppController],
   providers: [AppService],
 })
